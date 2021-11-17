a3f127a70bb1d653fcf99ad2a859e723f9270571
Jill Eliceiri <jhemmila@madisoncollege.edu>
2021-11-16 19:09:41 -0600

    updating pom, API.java, and index.html for deploy to AWS

--- src/main/java/com/teamcatlady/api/API.java
+++ src/main/java/com/teamcatlady/api/API.java
@@ -4,6 +4,8 @@ import io.swagger.jaxrs.config.BeanConfig;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
@@ -18,17 +20,25 @@ public class API extends Application {
     /**
      * Instantiates a new Api.
      */
-    public API() {
-        init();
-    }
 
-    private void init() {
+    public API() {
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.0");
+        beanConfig.setVersion("1.0.2");
         beanConfig.setTitle("Presidents API");
-        beanConfig.setBasePath("/teamCatLady_war/api");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("teamcatladypresidentsrestfulapi-env.eba-3avvmewu.us-east-2.elasticbeanstalk.com/");
+        beanConfig.setBasePath("/api");
+        //beanConfig.setBasePath("/teamCatLady_war/api");
         beanConfig.setResourcePackage(API.class.getPackage().getName());
         beanConfig.setScan(true);
     }
 
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet();
+        resources.add(com.teamcatlady.api.PresidentAPI.class);
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return resources;
+    }
 }
